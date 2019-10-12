b2d149f858a8fb76a57fb851e0845c96dd0f486e
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-10-12 17:04:37 +0200

    jesus christ is my saviour

---
+++ Assets/Levels/TestLevel/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private float openHeight = 3.0f;
+    [SerializeField]
+    private float cloesdHeight = 0.0f;
+
+    private void FixedUpdate()
+    {
+        Vector3 currPosition = transform.position;
+        currPosition.y = Mathf.Lerp(closedHeight, openHeight, )
+    }
+}
---
+++ Assets/Levels/TestLevel/DoorButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    [SerializeField]
+    private Door door = null;
+
+    private void OnTriggerEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerMovement>())
+
+    }
+}
