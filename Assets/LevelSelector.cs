d636f37247f0eb273a7fb3ff75e67dc6d1b22240
AlessioBona <alefbona@gmail.com>
2019-10-12 18:01:14 +0200

    LevelSelector seem to work

--- Assets/LevelSelector.cs
+++ Assets/LevelSelector.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Doozy.Engine.UI;
+//using UnityEditor.UI;
 
 
 public class LevelSelector : MonoBehaviour
 {
 
     [SerializeField]
-    GameObject LevelButtonPrefab;
+    GameObject levelButtonPrefab;
 
     [SerializeField]
     List<string> scenesNames;
+    [SerializeField]
     List<string> levelsNames;
+
+    List<GameObject> buttonsList;
+
+    [SerializeField]
+    GameObject buttonsParent;
+
+    [SerializeField]
+    UIView LevelSelectorView;
 
     private void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
 
         if(scenesNames == null)
         {
@@ -31,6 +43,18 @@ public class LevelSelector : MonoBehaviour
 
     public void UpdateLevelSelector()
     {
+        buttonsList = new List<GameObject>();
+        for (int i = 0; i < scenesNames.Count; i++)
+        {
+            GameObject newButton = Instantiate(levelButtonPrefab, buttonsParent.transform);
+            newButton.GetComponent<LevelSelectButton>().sceneName = scenesNames[i];
+            newButton.GetComponentInChildren<Button>().onClick.AddListener(newButton.GetComponent<LevelSelectButton>().LoadTheScene);
+            newButton.GetComponentInChildren<Button>().onClick.AddListener(CloseLevelSelect);
+        }
+    }
 
+    public void CloseLevelSelect()
+    {
+        LevelSelectorView.Hide();
     }
 }
