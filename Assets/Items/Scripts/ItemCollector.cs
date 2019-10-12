f55c670e2551fd81d0e8e5f7374250e9cfd6100a
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-10-12 15:01:18 +0200

    Add basic item collection system

---
+++ Assets/Items/Scripts/ItemCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    //everytime we're colliding with a trigger collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check whether the item we collided with has an item component
+        Item nextItem = collision.gameObject.GetComponent<Item>();
+        if(nextItem)
+        {
+            print(nextItem.GetItemType());
+            //depending on the item type
+            switch(nextItem.GetItemType())
+            {
+                case Item.Type.clone:
+                    break;
+            }
+
+            //destroy the item
+            Destroy(nextItem.gameObject);
+        }
+    }
+}
