20ac1bd189573b1f069956fcc7685e35034c75a2
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-10-12 19:47:25 +0200

    Add level manager to level4 to reload and load target levels

---
+++ Assets/Level Management/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private string targetLevel = "";
+
+    public void ReloadLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadTargetLevel ()
+    {
+        SceneManager.LoadScene(targetLevel);
+    }
+}
---
+++ Assets/Levels/Level4/Scripts/ExitDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+    [SerializeField]
+    private LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+            levelManager.LoadTargetLevel();
+    }
+}
