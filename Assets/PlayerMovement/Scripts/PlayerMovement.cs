e18d5527158d1d4adccf2cdbed99c960b188de10
Oussama Bouanani <oussama.bouanani@protonmail.com>
2019-10-12 20:33:14 +0200

    Add jump audio effect trigger

--- Assets/PlayerMovement/Scripts/PlayerMovement.cs
+++ Assets/PlayerMovement/Scripts/PlayerMovement.cs
@@ -22,9 +22,15 @@ public class PlayerMovement : MonoBehaviour
 
     private CloneState state;
 
+    //audio effects:
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip jumpAudio = null;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         state = CloneState.active;
     }
 
@@ -39,7 +45,11 @@ public class PlayerMovement : MonoBehaviour
 
                 //check whether the player is on the ground and whether the jump key is pressed to update the player's velocity on the y-axis (= jump)
                 if (Input.GetKeyDown(KeyCode.Space) && groundCheck.canJump)
+                {
                     this._rigidbody.velocity = new Vector2(this._rigidbody.velocity.x, jumpForce);
+                    audioSource.PlayOneShot(jumpAudio);
+                }
+                    
 
                 break;
         }
